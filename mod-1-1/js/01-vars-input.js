123de7c659e3d8d52f5249f00fb18555a55aa7bd
Elena Karmanovich <elkarmanovich@gmail.com>
2022-12-20 00:37:34 +0200

    переменные и типы

---
+++ mod-1-1/js/01-vars-input.js
@@ -0,0 +1,47 @@
+/*
+ * - Переменные и типы примитивов
+ * - const и let
+ * - Логирование с методом console.log()
+ * - Уникальность идентификатора
+ */
+
+const age = 10;
+const totalPrice = 200.74;
+const userName = 'Chelsy';
+const message = 'Добро пожаловать!';
+const isOpen = true;
+const shouldConfirm = false;
+
+// console.log(50);
+
+/*
+ * * - Оператор typeof
+ */
+
+// const type = typeof isOpen;
+// console.log(type);
+
+// const type1 = typeof age;
+// console.log(type1);
+
+/*
+ * - Как window.alert() блокирует интерпретацию
+ */
+
+// console.log('До');
+// alert('qweqweqwe');
+// console.log('После');
+
+//--------------------------------------------------
+/*
+ * - Ввод пользовователя
+ * - Методы window.confirm() и window.prompt()
+ */
+
+// const shouldRenew = confirm('Хотите продлить подписку?');
+// console.log(shouldRenew);
+
+// let quantity = prompt('Введите количество товаров');
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
